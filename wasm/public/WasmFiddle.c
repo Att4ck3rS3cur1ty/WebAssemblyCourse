3ba7a1b4fcaba1674fab4d6ac7e3b440e5dbc735
unknown <abimaelima54@gmail.com>
2020-03-28 00:20:58 -0300

    new way of accessing memory

---
+++ wasm/public/WasmFiddle.c
@@ -0,0 +1,17 @@
+#include <string.h>
+
+void numLog(int n);
+void strLog(char * offset, length);
+
+int main(){
+  return 42;
+}
+
+void greet(){
+  char * msg = "Hello From C!";
+  strLog(msg, strlen(msg));
+}
+
+void getDoubleNumber(int x){
+  numLog(x*2);
+}
