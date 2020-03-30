f9269c01fe5aff4fe60cce7101f2e7a0972abd60
unknown <abimaelima54@gmail.com>
2020-03-30 16:51:03 -0300

    debugging

--- wasm/lib/strings.c
+++ wasm/lib/strings.c
@@ -6,6 +6,15 @@ char * getStr(){
   return str;
 }
 
+int getNumber(){
+  int num = 22;
+  if (num < 30) {
+    emscripten_log(EM_LOG_WARN, "'num less than 30'");
+  }
+  // emscripten_debugger();
+  return num;
+}
+
 int main(){
   printf("WASM Ready\n");
   return 1;
