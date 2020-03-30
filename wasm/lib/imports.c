7241c454136133641f2fb2f4bd98e14c10c8b107
unknown <abimaelima54@gmail.com>
2020-03-30 16:27:12 -0300

    Calling JavaScript functions from C

--- wasm/lib/imports.c
+++ wasm/lib/imports.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
-#include <string.h>
+#include <emscripten.h>
+
+// Declare a reusable JS function
+EM_JS(void, jsFunction, (int n),{
+  console.log("Call from EM_JS: " + n);
+});
 
 int main(){
-  printf("WASM Ready - Testing with Emscripten HTML\n");
+  printf("WASM Ready\n");
+
+  // Call a JS function (eval)
+  emscripten_run_script("console.log('Hello from C!')");
+
+  // Call a JS function ASYNC (eval)
+  emscripten_async_run_script("console.log('Hello from C - ASYNC!')", 2000);
+
+  // Get return valuer from JS function
+  int jsVal = emscripten_run_script_int("getNum()");
+
+  // Get return valuer from JS function
+  char * jsValStr = emscripten_run_script_string("getStr()");
+
+  printf("Val from getNum(): %d\n", jsVal);
+    printf("Val from getStr(): %s\n", jsValStr);
+
+  // Execute EM_JS jsFunction
+
+  jsFunction(144);
 
   return 1;
-}
-
-int getNum(){
-  return 22;
-}
-
-int getDoubleNum(int n){
-  return n*2;
-}
-
-char * greet(char * name){
-  char * greeting = "Hello ";
-  strcat(greeting, name);
-  return greeting;
 }
